e4fb5e3d5a4dd63aade59da1d026ff7e1df7c773
S4JN <93881028+S4JN@users.noreply.github.com>
2022-08-09 19:24:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,43 +3,78 @@ const { rmSync } = require("fs");
 const app = express();
 const path = require("path");
 const axios = require("axios");
-
+const bodyParser =require("body-parser");
+const moment =require("moment");
 ///console.log(path.join(__dirname+"../public"));
-
-const staticPath = path.join(__dirname, "./public");
-
-
-console.log(staticPath);
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname +"/public"));
+app.use(express.static('views/images')); 
 
 
 
-// app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, staticPath)));
 
-app.post("/loc", async (req, res) => {
+
+
+
+app.post("/", async (req, res) => {
+
+    // try {
+    //     console.log("gj");
+    //     const location = req.body.location;
+    //     console.log(location);
+    //     const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=d7d863b6da1ba9f8d6246f511238105b`)
+    //     const data = result.data;
+    //     console.log(data);
+    //     res.send(data);
+
+    // } catch (error) {
+    //     console.log("err")
+    //     res.send(error);
+        
+    // }
+
 
     try {
-        console.log("gj");
-        const location = req.body.location;
-        console.log(location);
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=d7d863b6da1ba9f8d6246f511238105b`)
+        const myCity =req.body.location;
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=d7d863b6da1ba9f8d6246f511238105b`);
         const data = result.data;
-        console.log(data);
-        res.send(data);
 
+
+
+
+
+        // assigning values
+
+            var temperature =parseFloat(((data ['main'] ['temp'] ) - 273)).toFixed(2) ;
+            var weaDesp = (data ['weather'] [0] ['main']);
+            var humidity = (data ['main'] ['humidity']);
+            var country = (data ['sys'] ['country'] );
+            var city = (data ['name']);
+            //console.log(data);
+            console.log(temperature);
+            console.log(weaDesp);
+            console.log(humidity);
+            console.log(country);
+            console.log(city);
+
+            var isSunny = false;
+            var isRain =false;
+            if (weaDesp =="Sunny") {
+                isSunny=true;
+            } else if (weaDesp=="Rain") {
+                isRain=true;
+            }
+            
+
+            res.render('index',{temperature,isSunny,isRain,weaDesp,humidity,country,city,"date":moment(Date.now()).format('DD-MM-YYYY')});
+            
+    
     } catch (error) {
-        console.log("err")
-        res.send(error);
-        
+        res.render("index",{"msg":"enter a valid location"})
     }
-
-
-
-
-
+    
 
 
 });
@@ -53,6 +88,8 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
     res.writeHead(404);
     res.end("404 error page not found");
+    //render 404 page
+
 })
 
 app.listen("3000", () => {
